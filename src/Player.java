4667fff757234f14f4f65c303f6751fd96a334a3
wataru <w.furusato.sys23@morijyobi.ac.jp>
2024-07-10 10:30:55 +0900

    print1完了 closed #6 #7 #8

---
+++ src/Player.java
@@ -0,0 +1,29 @@
+public class Player {
+
+
+        private String playerName;
+
+        public Player(String playerName) {
+
+            this.playerName = playerName;
+
+        }
+
+        public String getPlayerName() {
+
+            return playerName;
+
+        }
+
+        public void setPlayerName(String playerName) {
+
+            this.playerName = playerName;
+
+        }
+
+    }
+
+
+
+
+

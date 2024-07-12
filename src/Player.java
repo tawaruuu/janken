dfd69dc1891fcbe62765eef277438099b91f169b
wataru <w.furusato.sys23@morijyobi.ac.jp>
2024-07-12 11:49:13 +0900

    ユーザ入力で手を決める処理を実装した close #9, close #10, close #11

---
+++ src/Player.java
@@ -0,0 +1,46 @@
+package src;
+
+
+public class Player {
+
+    private String playerName;
+
+    private Hand hand;
+
+
+
+    public Player(String playerName) {
+
+        this.playerName = playerName;
+
+    }
+
+    public String getPlayerName() {
+
+        return playerName;
+
+    }
+
+    public void setPlayerName(String playerName) {
+
+        this.playerName = playerName;
+
+    }
+
+
+
+    public Hand getHand() {
+
+        return this.hand;
+
+    }
+
+
+
+    public void setHand(Hand hand) {
+
+        this.hand = hand;
+
+    }
+
+}
